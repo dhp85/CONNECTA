5f779f7bb01cc758b50e3cbc0e53add1eb1d0582
Diego Herreros <diegohp85@gmail.com>
2024-07-29 13:14:51 +0200

    Feat: Implementar find_strike

--- list_utils.py
+++ list_utils.py
@@ -59,3 +59,14 @@ def find_n(list, needle, n):
     else:
         return False
        
+def find_streak(list, needle, nstrike):  
+    """
+    Devuelve True si en lista hay n o mas needles seguidos False, para todos los demas.
+    """
+    # si n >= 0.
+    # inicializo el indice el contador y el indicador de racha.
+    # mientras no haya encontrado n seguidos y la lista no se haya acabado.
+    # si lo encuentro, activo el indicador de rachas y actualizo el contador.
+    # si no lo encuentro, desactivo indicador de racha y pongo a cero el contador.
+    # avanzo al siguiente elemento.
+    # devolvemos el resultado de comparar el contador con n siempre y cuando estemos en racha     
